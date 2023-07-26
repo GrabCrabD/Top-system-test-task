bffa90cbdcbaa2dc638719067bb71b296d02bf11
GrabCrabD <105257214+GrabCrabD@users.noreply.github.com>
2023-07-26 12:31:41 +0400

    added exception checking for creation of figures with non-positive side lengths (radius) and attempt to create a triangle with non-existent sides

--- src/shapes.hpp
+++ src/shapes.hpp
@@ -1,5 +1,6 @@
 #include <cmath>
 #include <iostream>
+#include <stdexcept>
 
 using namespace std;
 
@@ -14,7 +15,11 @@ class Circle : public Shape {
   double radius;
 
  public:
-  Circle(double r) : radius(r) {}
+  Circle(double r) : radius(r) {
+    if (radius <= 0) {
+      throw invalid_argument("Circle radius must be greater than zero.");
+    }
+  }
   ~Circle() override {}
   void draw() const override {
     cout << "Drawing a circle with radius " << radius << endl;
@@ -26,20 +31,30 @@ class Square : public Shape {
   double side;
 
  public:
-  Square(double s) : side(s) {}
+  Square(double s) : side(s) {
+    if (side <= 0) {
+      throw invalid_argument("Square side length must be greater than zero.");
+    }
+  }
   ~Square() override {}
   void draw() const override {
     cout << "Drawing a square with side " << side << endl;
   }
 };
 
-// Класс для треугольника
 class Triangle : public Shape {
  private:
   double side1, side2, side3;
 
  public:
-  Triangle(double s1, double s2, double s3) : side1(s1), side2(s2), side3(s3) {}
+  Triangle(double s1, double s2, double s3) : side1(s1), side2(s2), side3(s3) {
+    if (side1 <= 0 || side2 <= 0 || side3 <= 0 || side1 + side2 <= side3 ||
+        side1 + side3 <= side2 || side2 + side3 <= side1) {
+      throw invalid_argument(
+          "Invalid sides for triangle. All sides must be greater than zero and "
+          "satisfy the triangle inequality.");
+    }
+  }
   ~Triangle() override {}
   void draw() const override {
     cout << "Drawing a triangle with sides " << side1 << ", " << side2 << ", "
--- src/shapes_tests.cpp
+++ src/shapes_tests.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include <stdexcept>
+
 #include "shapes.hpp"
 
 TEST(ShapesTest, Constructor1) {
@@ -12,20 +14,41 @@ TEST(ShapesTest, Constructor1) {
 }
 TEST(ShapesTest, Constructor2) {
   Shape* square = new Square(4.0);
-  testing::internal::CaptureStdout();  // Захват вывода на экран
+  testing::internal::CaptureStdout();
   square->draw();
   std::string output = testing::internal::GetCapturedStdout();
-  delete square;  // удалить объект, чтобы избежать утечек памяти
+  delete square;
   EXPECT_EQ(output, "Drawing a square with side 4\n");
 }
 
 TEST(ShapesTest, Constructor3) {
   Shape* triangle = new Triangle(3.0, 4.0, 5.0);
-  testing::internal::CaptureStdout();  // Захват вывода на экран
+  testing::internal::CaptureStdout();
   triangle->draw();
   std::string output = testing::internal::GetCapturedStdout();
-  delete triangle;  // удалить объект, чтобы избежать утечек памяти
+  delete triangle;
   EXPECT_EQ(output, "Drawing a triangle with sides 3, 4, 5\n");
+}
+
+TEST(ShapesTest, Constructor1_Error) {
+  Shape* circle = nullptr;
+  ASSERT_THROW(circle = new Circle(-5.0), std::invalid_argument);
+  delete circle;
+}
+
+TEST(ShapesTest, Constructor2_Error) {
+  Shape* square = nullptr;
+  ASSERT_THROW(square = new Square(0), std::invalid_argument);
+  ASSERT_THROW(square = new Square(-4.0), std::invalid_argument);
+  delete square;
+}
+
+TEST(ShapesTest, Constructor3_Error) {
+  Shape* triangle = nullptr;
+  ASSERT_THROW(triangle = new Triangle(3.0, 4.0, 8.0), std::invalid_argument);
+  ASSERT_THROW(triangle = new Triangle(3.0, -4.0, 5.0), std::invalid_argument);
+  ASSERT_THROW(triangle = new Triangle(0.0, 4.0, 5.0), std::invalid_argument);
+  delete triangle;
 }
 
 int main(int argc, char** argv) {
